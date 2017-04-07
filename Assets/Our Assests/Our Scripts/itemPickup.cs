051d6a896aa6a201370c23f2ce4a05007ff6e7db
LittleFishies <shotlock020@gmail.com>
2017-04-07 16:35:56 -0700

    Bringin in buildings,,,,,,,
    Just testing them out a little. I think they'd do fine in the snowy
    scene???? I have no clue if they'll merge well or not.

---
+++ Assets/Our Assests/Our Scripts/itemPickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemPickup : MonoBehaviour {
+
+    public bool ConsumeItem = false;
+    public Collider itemGrab;
+    //public float itemMid;
+    public float itemRadius = 5;
+
+	// Use this for initialization
+	void Start () {
+        //itemRadius = Physics.OverlapSphere(transform.position, itemRadius, int layerMask, QueryTriggerInteraction.UseGlobal);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
